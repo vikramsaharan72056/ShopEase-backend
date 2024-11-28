4673e6c1df89e2abf026ba546bd8f9afd5a0db42
Piyushwadhwa0123 <Piyushwadhwa0123@gmail.com>
2024-11-28 10:34:41 +0530

    cors values modified

--- server.js
+++ server.js
@@ -15,8 +15,23 @@ const app = express();
 // Middleware
 app.use(cors());
 //whitelist my frontends
-app.use(cors({ origin: 'https://shopease-frontend.onrender.com' }, { origin: 'http://localhost:3000' }));
-app.use(bodyParser.json());
+
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://shopease-frontend.onrender.com",
+    
+  ];
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true); // Allow request
+        } else {
+          callback(new Error("Not allowed by CORS")); // Block request
+        }
+      },
+    })
+  );app.use(bodyParser.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
